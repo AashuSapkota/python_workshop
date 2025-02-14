4f3bb0f042a3a33aa88acd7af0a1deede443349b
aashu <sapkotasabin24@gmail.com>
2025-02-14 20:37:20 +0545

    crud operation handeled for artist app using django forms

---
+++ django_prc/artist_management/artists/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.artist_list, name='artist_list'),
+    path('artist/<int:pk>/', views.artist_detail, name='artist_detail'),
+    path('artist/new/', views.artist_create, name='artist_create'),
+    path('artist/<int:pk>/edit/', views.artist_update, name='artist_update'),
+    path('artist/<int:pk>/delete/', views.artist_delete, name='artist_delete'),
+]
--- django_prc/artist_management/artists/views.py
+++ django_prc/artist_management/artists/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Artists
+from .forms import ArtistForm
 
-# Create your views here.
+def artist_list(request):
+    artists = Artists.objects.all()
+    return render(request, 'artists/artist_list.html', {'artists': artists})
+
+def artist_detail(request, pk):
+    artist = get_object_or_404(Artists, pk=pk)
+    return render(request, 'artists/artist_detail.html', {'artist': artist})
+
+def artist_create(request):
+    if request.method == 'POST':
+        form = ArtistForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('artist_list')
+    else:
+        form = ArtistForm()
+    return render(request, 'artists/artist_form.html', {'form': form})
+
+def artist_update(request, pk):
+    artist = get_object_or_404(Artists, pk=pk)
+    if request.method == 'POST':
+        form = ArtistForm(request.POST, instance=artist)
+        if form.is_valid():
+            form.save()
+            return redirect('artist_list')
+    else:
+        form = ArtistForm(instance=artist)
+    return render(request, 'artists/artist_form.html', {'form': form})
+
+def artist_delete(request, pk):
+    artist = get_object_or_404(Artists, pk=pk)
+    if request.method == 'POST':
+        artist.delete()
+        return redirect('artist_list')
+    return render(request, 'artists/artist_confirm_delete.html', {'artist': artist})
